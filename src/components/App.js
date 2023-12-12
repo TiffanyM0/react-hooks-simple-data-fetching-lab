8212169e64fe34a5cd3bdedcc8dfbfcdf84a684f
TIFFANY MAINA <tiffmaina@gmail.com>
2023-12-12 14:24:39 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+export default function App(){
+    const [image, setImage]= useState(null)
+    useEffect(()=>{
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then((data)=>{
+            console.log(data);
+            setImage(data.message);
+        })
+    }, [])
+
+    if(!image) {
+        return <p>Loading</p>
+    }
+    return (
+        <div>
+            <img alt="A Random Dog"  src={image}/>
+        </div>
+    )
+}
